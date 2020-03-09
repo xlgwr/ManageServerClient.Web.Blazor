ceab01c1024d27edf3376564b88cd30dcc3c82bd
xielinggen <linggen.xie@yintech.cn>
2020-03-09 10:01:23 +0800

    add IJSRuntime js

---
+++ ManageServerClient.Web.Blazor/jsRuntimeHelper/jsRuntimeExt.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class jsRuntimeExt
+    {
+        /// <summary>
+        /// bootstrap  模态框 modal.js
+        /// </summary>
+        /// <param name="jsRuntime"></param>
+        /// <param name="id"></param>
+        /// <param name="options"></param>
+        public static void modalJs(this IJSRuntime jsRuntime, string id, string options, string funName = "modalJs")
+        {
+            jsRuntime.InvokeVoidAsync($"exampleJsFunctions.{funName}", id, options);
+        }
+    }
+}
---
+++ ManageServerClient.Web.Blazor/wwwroot/js/customers.js
@@ -0,0 +1,21 @@
+﻿// Shorthand for $( document ).ready()
+$(function () {
+
+    function modalJs(id, options) {
+        console.log("modalJs");
+        console.log(id);
+        console.log(options);
+        var idto = '#' + id;
+        $(idto).modal(options);
+    }
+
+
+    //用于C# IJSRuntime 调用
+    window.exampleJsFunctions = {
+        showPrompt: function (message) {
+            return prompt(message, 'Type anything here');
+        },
+        modalJs: modalJs
+    };
+
+});
